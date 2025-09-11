71a9653646bc7b6dbf899d194243de4ee580ec4b
Clare MacAdie <clare@macadie.co.uk>
2025-09-11 16:30:27 +0100

    CSS for schedule list

--- view/viewList.js
+++ view/viewList.js
@@ -7,6 +7,7 @@ export default class ViewList {
   #init() {
     this.$ul = document.createElement('ul');
     this.#createHTML();
+    this.#configureHTML();
   }
 
   // ---------- public API ----------
@@ -16,6 +17,10 @@ export default class ViewList {
     this.app.allSchedules.forEach(schedule => {
       this.$ul.append(schedule.$li);
     });
+  }
+
+  #configureHTML() {
+    this.$ul.classList.add('schedule-list')
   }
 
   // ---------- helpers ----------
